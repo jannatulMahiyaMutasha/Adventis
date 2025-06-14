68444eb8f50ce2347d280520b09f14de8d3af1d0
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2025-06-14 22:15:07 +0600

    add code

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,10 @@
 import { useState, useEffect } from "react";
 
-// Custom hook to check if the user is authenticated
 export const useAuth = () => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
 
   useEffect(() => {
-    // Retrieve user and token from localStorage
     const storedUser = JSON.parse(localStorage.getItem("user"));
     const storedToken = localStorage.getItem("token");
 
