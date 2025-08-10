34a64df29bb7012fcd1801d322099e6dceb3bc6d
Jannatul Mahiya Mutasha <157698734+jannatulMahiyaMutasha@users.noreply.github.com>
2025-08-10 18:12:12 +0600

    login code edit

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export default function GoogleLogin() {
   const navigate = useNavigate();
 
-  // Handle Google Login
+  // Handle Google Login code
   const handleGoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
